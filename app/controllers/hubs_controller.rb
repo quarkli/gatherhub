8e7c871fa508e5149db061b4997131dbb1a9f284
Phenix <phenix.cai@gmail.com>
2015-09-21 03:31:29 +0000

    [PC] add create a hub page

--- app/controllers/hubs_controller.rb
+++ app/controllers/hubs_controller.rb
@@ -5,4 +5,28 @@ class HubsController < ApplicationController
   
   def create
   end
+
+  def show
+    @hub = Hub.find(params[:id])
+    #debugger
+  end
+
+  def create
+    @hub = Hub.new(hub_params) 
+    if @hub.save
+      #log_in @hub
+      flash[:success] = "Welcome to the Your Hub!"
+      #redirect_to @hub
+      redirect_to hub_url(@hub)
+    else
+     render 'new'
+    end
+  end
+
+
+private
+
+  def hub_params
+    params.require(:hub).permit(:name, :email, :password)
+  end  
 end
