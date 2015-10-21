81fc196f8b6672841ad0dc37cbc597ef95ed1d45
Quark Li <quarkli@gmail.com>
2015-10-21 11:36:24 +0800

    Minor modification in class style

--- public/gatherhub.teleboard.js
+++ public/gatherhub.teleboard.js
@@ -8,83 +8,21 @@ function precision(num, p) {
 }
 
 (function(){
+	SvgCanvas.prototype.z = 1;  // zoom ratio, 0.1 <= z <= 10
+	SvgCanvas.prototype.m = 5;  // canvas margin
+	SvgCanvas.prototype.x = 0;  // canvas viewBox_x
+	SvgCanvas.prototype.y = 0;  // canvas viewBox_y
+	SvgCanvas.prototype.w = 0;  // canvas viewBox_w
+	SvgCanvas.prototype.h = 0;  // canvas viewBox_h
+	SvgCanvas.prototype.canvas;
 
-	function SvgCanvas(w, h) {
-		this.z = 1;
-		this.m = 5;
-		this.x = 0;
-		this.y = 0;
-		this.w = w | window.innerWidth - this.m;
-		this.h = h | window.innerHeight - this.m;
-		this.canvas = $(document.createElementNS('http://www.w3.org/2000/svg', 'svg'));
-		this.fit(w, h);
-	}
-	
-	SvgCanvas.prototype.inserInto = function(elem) {
-		if ($(elem)) {
-			$(elem).append(this.canvas);
-			return true;
-		}
-		return false;
-	}
-
-	SvgCanvas.prototype.width = function(w) {
-		if (w > 0) {
-			this.w = w;
-			this.canvas.attr('width', this.w - this.m);
-		}
-		return this.canvas.attr('width');
-	}
-
-	SvgCanvas.prototype.height = function(h) {
-		if (h > 0) {
-			this.h = h;
-			this.canvas.attr('height', this.h - this.m);
-		}
-		return this.canvas.attr('height');
-	}
-
-	SvgCanvas.prototype.margin = function(m) {
-		if (m > 0) {
-			this.m = m;
-		}
-		return this.m;
-	}
-
-	SvgCanvas.prototype.offset = function(axis, offset) {
-		if ($.isNumeric(offset)) {
-			if (axis == 'x') this.x += offset;
-			if (axis == 'y') this.y += offset;
-			this.canvas[0].setAttribute('viewBox', precision(this.x, 3) + ' ' + precision(this.y, 3) + ' ' + precision(this.w,3) + ' ' + precision(this.h,3));
-		}
-	}
-
-	SvgCanvas.prototype.fit = function(pw, ph) {
-		this.w = pw || window.innerWidth - this.m;
-		this.h = ph || window.innerHeight - this.m;
-		this.canvas.attr('width', this.w);
-		this.canvas.attr('height',this.h);
-		this.canvas[0].setAttribute('viewBox', precision(this.x, 3) + ' ' + precision(this.y, 3) + ' ' + precision(this.w, 3) + ' ' + precision(this.h, 3));
-	}
-
-	SvgCanvas.prototype.zoom = function(z) {
-		if (z >= 0.1 && z <= 10) {
-			this.w = this.canvas.attr('width') / z;
-			this.h = this.canvas.attr('height') / z;
-			this.x = (this.canvas.attr('width') - this.w) / 2;
-			this.y = (this.canvas.attr('height') - this.h ) / 2;
-			this.z = z;
-			// when there are upper case letter in attribute name, 
-			// we must use native javascript setAttribute() instead of 
-			// JQuery .attr() which always convert attribute name to lower case
-			this.canvas[0].setAttribute('viewBox', precision(this.x,3) + ' ' + precision(this.y,3) + ' ' + precision(this.w,3) + ' ' + precision(this.h,3));
-		}
-		return this.z;
+	SvgCanvas.prototype.toString = function() {
+		return this.canvas.html();
 	}
 
 	SvgCanvas.prototype.calibration = function() {
-		var w = this.canvas.attr('width');
-		var h = this.canvas.attr('height');
+		var w = this.width();
+		var h = this.height();
 		var path = $(document.createElementNS('http://www.w3.org/2000/svg', 'path'));
 		path.attr('id', 'grid');
 		path.attr('fill', 'none');
@@ -98,8 +36,64 @@ function precision(num, p) {
 		this.canvas.append(path);
 	}
 
-	SvgCanvas.prototype.toString = function() {
-		return this.canvas.html();
+	SvgCanvas.prototype.appendTo = function(elem) {
+		if ($(elem)) $(elem).append(this.canvas);
+	}
+
+	SvgCanvas.prototype.offset = function(axis, offset) {
+		if ($.isNumeric(offset)) {
+			if (axis == 'x') this.x = precision(this.x + offset, 3);
+			if (axis == 'y') this.y = precision(this.y + offset, 3);
+			// when there are upper case letter in attribute name, 
+			// we must use native javascript setAttribute() instead of 
+			// JQuery .attr() which always convert attribute name to lower case
+			this.canvas[0].setAttribute('viewBox', this.x + ' ' + this.y + ' ' + this.w + ' ' + this.h);
+		}
+		if (axis == 'x') return this.x;
+		if (axis == 'y') return this.y;
+	}
+
+	SvgCanvas.prototype.zoom = function(z) {
+		if (z >= 0.1 && z <= 10) {
+			this.w = precision(this.w / z, 3);
+			this.h = precision(this.h / z, 3);
+			this.x = precision((this.width() - this.w) / 2, 3);
+			this.y = precision((this.height() - this.h ) / 2, 3);
+			this.z = z;
+			// when there are upper case letter in attribute name, 
+			// we must use native javascript setAttribute() instead of 
+			// JQuery .attr() which always convert attribute name to lower case
+			this.canvas[0].setAttribute('viewBox', this.x + ' ' + this.y + ' ' + this.w + ' ' + this.h);
+		}
+		return this.z;
+	}
+
+	SvgCanvas.prototype.margin = function(m) {
+		if (m > 0) this.m = m;
+		return this.m;
+	}
+
+	SvgCanvas.prototype.width = function(w) {
+		if (w - this.m > 0) this.canvas.attr('width', precision(w - this.m, 3));
+		return this.canvas.attr('width');
+	}
+
+	SvgCanvas.prototype.height = function(h) {
+		if (h - this.m > 0) this.canvas.attr('height', precision(h - this.m, 3));
+		return this.canvas.attr('height');
+	}
+
+	SvgCanvas.prototype.fit = function(pw, ph) {
+		var w = pw || window.innerWidth;
+		var h = ph || window.innerHeight;
+		this.w = this.width(w);
+		this.h = this.height(h);
+		this.canvas[0].setAttribute('viewBox', this.x + ' ' + this.y + ' ' + this.w + ' ' + this.h);
+	}
+
+	function SvgCanvas(w, h) {
+		this.canvas = $(document.createElementNS('http://www.w3.org/2000/svg', 'svg'));
+		this.fit(w, h);
 	}
 	
 	// add class SvgCanvas to the namespace
@@ -107,8 +101,13 @@ function precision(num, p) {
 })();
 
 (function(){
-	function VisualPad(w, h){
+	function VisualPad(w, h, src){
 		gatherhub.SvgCanvas.call(this, w, h);
+		if ($(src)) this.src(src);
+		this.canvas.css('display', 'block');
+		this.canvas.css('position', 'absolute');
+		this.canvas.css('top', '0px');
+		this.canvas.css('left', '0px');
 	}
 	
 	VisualPad.prototype = new gatherhub.SvgCanvas();
