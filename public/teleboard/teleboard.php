c0ca55f97cbea3200717a6c88c165035df7f2c54
Quark Li <quarkli@gmail.com>
2015-11-24 12:21:50 +0800

    Change email notification to be optional

--- public/teleboard/teleboard.php
+++ public/teleboard/teleboard.php
@@ -5,7 +5,7 @@ if(isset($_POST['hub'])) $hub = $_POST['hub'];
 if(isset($_POST['peer'])) $peer = $_POST['peer'];
 if (isset($_GET['hub'])) $hub = $_GET['hub'];
 if (isset($_GET['peer'])) $peer = $_GET['peer'];
-if(isset($_POST['email'])) {
+if(isset($_POST['email']) && isset($_POST['sendMail'])) {
 	$to      = $_POST['email'];
 	$subject = 'Gatherhub: Your hub is created';
 	$headers = 'From: qlli@gatherhub.com' . "\r\n" .
@@ -54,43 +54,89 @@ Author: quarkli@gmail.com
     <script src="js/bootstrap.min.js"></script>
     <script src="scripts/gatherhub.js"></script>
     <script src="scripts/svgicons.js"></script>
+    <script src="scripts/jquery.nicescroll.min.js"></script>
     <script src="scripts/teleboard.js"></script>
     <script>
       var hub = '<?= $hub ?>';
       var peer = '<?= $peer ?>';
-   </script>
+    </script>
     <style>
-    .layer {
-      position: absolute;
-    }
-    </style>
+	.scrollarea {
+		background-color: auto;
+		display: none;
+		width: 250px;
+		height: 100%;
+		float: left;
+		overflow-y: auto;
+		border-style: solid;
+		border-width: 1px;
+		border-top-right-radius:5px;
+		border-bottom-right-radius:5px;
+	}
+	.collapse.width {
+		display: block;
+		-webkit-transition: 0.5s ease-in-out;
+		-moz-transition: 0.5s ease-in-out;
+		-o-transition: 0.5s ease-in-out;
+		transition: 0.5s ease-in-out;
+	}
+	.btn {
+		padding: 5px;
+	}
+	#menu {
+		float: left;
+		height: 100%;
+		width: 60px;
+		font-size: 0px;
+		text-align: center;
+		margin: 0 auto;
+		background-color: #CCC;
+		overflow: hidden;
+	}
+	#list {
+		float: left;
+		height: 100%;
+		width: 250px;
+		font-size: 0px;
+		text-align: center;
+		background-color: auto;
+		overflow-x: hidden;
+		overflow-y: auto;
+		border-style: solid;
+		border-width: 1px;
+		border-top-right-radius:5px;
+		border-bottom-right-radius:5px;
+		background-color: #00C;
+	}
+	#board {
+		float: left;
+		height: 100%;
+		overflow: hidden;
+		background-color: #C00;
+	}
+	</style>
   </head>
   <body>
-  <div id="layer1" class="layer" style="width: 100%; height: 100%"></div> <!-- SketchPad -->
-  <div id="layer2" class="layer"></div> <!-- Informatio & Message -->
-  <div id="layer3" class="layer"></div> <!-- VisualPad -->
-  <div id="layer4" class="layer"></div> <!-- Toolbar -->
-  <div id="layer5" class="layer"></div> <!-- Chatbox -->
   <div id="joinhub" class="modal fade" role="dialog">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h4 class="modal-title">Join hub</h4>
-        </div>
-        <div class="modal-body">
+	<div class="modal-dialog">
+	  <div class="modal-content">
+		<div class="modal-header">
+		  <h4 class="modal-title">Join hub</h4>
+		</div>
+		<div class="modal-body">
 		  <div class="form-group">
-		    <label for="text">Your Name:</label>
-		    <input type="text" class="form-control" id="peer" name="peer">
+			<label for="text">Your Name:</label>
+			<input type="text" class="form-control" id="peer" name="peer">
 		  </div>
 		  <div class="form-group" style="text-align: right;">
-		    <div class="checkbox">
-		      <label><input type="checkbox" id="cacheOn">Remember me</label>
-		    </div>
-		      <button type="Button" class="btn btn-default" onclick="return validateInput()">OK</button>
+			<div class="checkbox">
+			  <label><input type="checkbox" id="cacheOn">Remember me</label>
+			</div>
+			  <button type="Button" class="btn btn-default" onclick="return validateInput()">OK</button>
 		  </div>
-        </div>
-      </div>
-    </div>
+		</div>
+	  </div>
+	</div>
   </div>
   </body>
 </html>
