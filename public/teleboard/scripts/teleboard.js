683af828d28d3b6b183b7094ea95732606d1c793
Quark Li <quarkli@gmail.com>
2015-12-11 16:07:28 +0800

    Modified Teleboard button menu to provide more functions

--- public/teleboard/scripts/teleboard.js
+++ public/teleboard/scripts/teleboard.js
@@ -28,21 +28,30 @@ $(function(){
 		return btn;
 	}
 	
-	var btnUser = addBtnToMenu({icon: svgicon.user, iconcolor: '#448', w: 40, h: 40, borderwidth: 2, bordercolor: '#448', borderradius: 1, bgcolor: '#FFF'}, '#plist');
-	var btnMsg = addBtnToMenu({icon: svgicon.chat, iconcolor: '#448', w: 40, h: 40, resize: .7, borderwidth: 2, bordercolor: '#448', borderradius: 1, bgcolor: '#FFF'}, '#msg');
-	var btnSpk = addBtnToMenu({icon: svgicon.mic, iconcolor: '#448', w: 40, h: 40, borderwidth: 2, bordercolor: '#448', borderradius: 1, bgcolor: '#FFF'}, '#media');
+	var btnUser = addBtnToMenu({tip: 'Peer List', icon: svgicon.user, iconcolor: '#448', w: 40, h: 40, borderwidth: 2, bordercolor: '#448', borderradius: 1, bgcolor: '#FFF'}, '#plist');
+	var btnMsg = addBtnToMenu({tip: 'Text Chatroom', icon: svgicon.chat, iconcolor: '#448', w: 40, h: 40, resize: .7, borderwidth: 2, bordercolor: '#448', borderradius: 1, bgcolor: '#FFF'}, '#msg');
+	var btnSpk = addBtnToMenu({tip: 'Speaker Control Panel', icon: svgicon.mic, iconcolor: '#448', w: 40, h: 40, borderwidth: 2, bordercolor: '#448', borderradius: 1, bgcolor: '#FFF'}, '#media');
+	var btnVP = addBtnToMenu({tip: 'Show/Hide View-window', icon: svgicon.picture, iconcolor: '#448', w: 40, h: 40, borderwidth: 2, bordercolor: '#448', borderradius: 1, bgcolor: '#FFF'}, '');
+	btnVP.onclick = function(){vp.pad.toggle();sp.attachvp(vp);};
 	
 	var sp = msp = new Gatherhub.SketchPad();
 	sp.floating('absolute').pencolor(sp.repcolor).penwidth(1).appendto('#pad');
 	sp.canvas.css('opacity', 0.75);
+	var selcolor = sp.repcolor;
 
-	var vp = mvp = new Gatherhub.VisualPad().moveto('left', 9999);
+	var vp = mvp = new Gatherhub.VisualPad();
 	vp.draggable = true;
 	vp.floating('absolute').bgcolor('#FFF').bordercolor('#888').borderwidth(3);
 	vp.defsize(sp.width()/4, sp.height()/4).minimize().appendto('#pad');
 
 	sp.attachvp(vp);
 	arrangemenu();
+	
+	function setPenColor(c) {
+		selcolor = c;
+		sp.pencolor(c);
+		if (sp.timode) sp.tibox.css('color', c);
+	}
 
 	var w = h = parseInt($(window).height() / 24) * 2;
 	var rootdir = 'v0';
@@ -53,27 +62,24 @@ $(function(){
 		w = h = parseInt($(window).width() / 24) * 2;
 	}
 	if (w < 40) w = h = 40;
-	var penList = [
-		{btn: {w: w, h: h, icon: svgicon.pen, iconcolor: sp.repcolor, tip: 'User Pen'}, act: function(){sp.drag(0);sp.pencolor(sp.repcolor);}},
-		{btn: {w: w, h: h, icon: svgicon.pen, tip: 'Black Pen'}, act: function(){sp.drag(0);sp.pencolor('black');}},
-		{btn: {w: w, h: h, icon: svgicon.pen, iconcolor: 'red', tip: 'Red Pen'}, act: function(){sp.drag(0);sp.pencolor('red');}},
-		{btn: {w: w, h: h, icon: svgicon.pen, iconcolor: 'green', tip: 'Green Pen'}, act: function(){sp.drag(0);sp.pencolor('green');}},
-		{btn: {w: w, h: h, icon: svgicon.pen, iconcolor: 'blue', tip: 'Blue Pen'}, act: function(){sp.drag(0);sp.pencolor('blue');}},
-		{btn: {w: w, h: h, icon: svgicon.eraser, tip: 'Eraser'}, act: function(){sp.drag(0);;sp.pencolor(sp.bgcolor());}},
-		{btn: {w: w, h: h, icon: svgicon.move, tip: 'Move Canvas'}, act: function(){sp.drag(1);}}
+	var colorpalette = [
+		{btn: {w: w, h: h, icon: svgicon.cpalette, iconcolor: sp.repcolor, tip: 'Peer Color'}, act: function(){setPenColor(sp.repcolor);}},
+		{btn: {w: w, h: h, icon: svgicon.cpalette, tip: 'Black'}, act: function(){setPenColor('black');}},
+		{btn: {w: w, h: h, icon: svgicon.cpalette, iconcolor: 'red', tip: 'Red'}, act: function(){setPenColor('red');}},
+		{btn: {w: w, h: h, icon: svgicon.cpalette, iconcolor: 'green', tip: 'Green'}, act: function(){setPenColor('green');}},
+		{btn: {w: w, h: h, icon: svgicon.cpalette, iconcolor: 'blue', tip: 'Blue'}, act: function(){setPenColor('blue');}}
 	];
-	var sizeList = [
-		{btn: {w: w, h: h, icon: svgicon.circle, resize: 0.15, tip: 'Small (1px)'},	act: function(){sp.txtedit(0);sp.penwidth(1)}},
-		{btn: {w: w, h: h, icon: svgicon.circle, resize: 0.45, tip: 'Midium (11px)'}, act: function(){sp.txtedit(0);sp.penwidth(11)}},
-		{btn: {w: w, h: h, icon: svgicon.circle, resize: 0.65, tip: 'Large (21px)'}, act: function(){sp.txtedit(0);sp.penwidth(21)}},
-		{btn: {w: w, h: h, icon: svgicon.textinput, resize: 1, tip: 'Text Input'},	act: function(){sp.txtedit(1);}}
+	var pensz = [
+		{btn: {w: w, h: h, icon: svgicon.brushl, resize: .45, tip: 'Thinner Paint'},	act: function(){sp.penwidth(1)}},
+		{btn: {w: w, h: h, icon: svgicon.brushl, tip: 'Thicker Paint'}, act: function(){sp.penwidth(21)}},
+		{btn: {w: w, h: h, icon: svgicon.brushl, resize: .65, tip: 'Regular Paint'}, act: function(){sp.penwidth(11)}}
 	];
-	var zoomList = [
-		{btn: {w: w, h: h, icon: svgicon.zoomout, tip: 'Zoom Out'},	act: function(){sp.zoom(sp.zrate / 1.1);}},
+	var zoomctrl = [
+		{btn: {w: w, h: h, icon: svgicon.zoomin, tip: 'Zoom In'}, act: function(){sp.zoom(sp.zrate * 1.1);}},
 		{btn: {w: w, h: h, icon: svgicon.fit, tip: 'Fit Content'}, act: function(){sp.fitcontent();}},
-		{btn: {w: w, h: h, icon: svgicon.zoomin, tip: 'Zoom In'}, act: function(){sp.zoom(sp.zrate * 1.1);}}
+		{btn: {w: w, h: h, icon: svgicon.zoomout, tip: 'Zoom Out'},	act: function(){sp.zoom(sp.zrate / 1.1);}}
 	];
-	var settingList = [
+	var canvasedit = [
 		{btn: {w: w, h: h, icon: svgicon.clear, tip: 'Clear Canvas'}, act: function(){
 			if (confirm('This will clear everything on the whiteboard of all peers. Are you sure?')) sp.clearall();
 		}},
@@ -81,15 +87,62 @@ $(function(){
 		{btn: {w: w, h: h, icon: svgicon.undo, tip: 'Undo'}, act: function(){sp.undo();}}
 	];	
 	var mainBtn = [
-		{sublist: penList, direction: subdir},
-		{sublist: sizeList, direction: subdir},
-		{btn: {w: w, h: h, icon: svgicon.zoom, tip: 'Zoom'}, sublist: zoomList, direction: subdir},
-		{btn: {w: w, h: h, icon: svgicon.setting, tip: 'Settings'},	sublist: settingList, direction: subdir}
+		{sublist: colorpalette, direction: subdir},
+		{sublist: pensz, direction: subdir},
+		{btn: {w: w, h: h, icon: svgicon.zoom, tip: 'Zoom'}, sublist: zoomctrl, direction: subdir},
+		{btn: {w: w, h: h, icon: svgicon.setting, tip: 'Settings'},	sublist: canvasedit, direction: subdir}
 	];
 	var rootList = {rootlist: mainBtn, direction: rootdir};
 	var toolBar = mbmenu = new Gatherhub.BtnMenu(rootList);
-	toolBar.root.css({'position': 'absolute', 'bottom': 0, 'right': 0});
-
+	if (topmenu) toolBar.root.css({'position': 'absolute', 'bottom': 0, 'right': 80});
+	else toolBar.root.css({'position': 'absolute', 'bottom': 80, 'right': 0});
+	
+	var actBtns = [
+		{btn: {w: w, h: h, icon: svgicon.pen, iconcolor: 'white', borderwidth: 3, bordercolor: 'white', borderradius: 2, bgcolor: 'red', resize: .6, tip: 'Free Hand Writing'}, act: function(){
+			sp.drag(0);
+			sp.txtedit(0);
+			toolBar.collapseall();
+			toolBar.root.children().show();
+			setPenColor(selcolor);
+		}},
+		{btn: {w: w, h: h, icon: svgicon.eraser, iconcolor: 'white', borderwidth: 3, bordercolor: 'white', borderradius: 2, bgcolor: 'red', resize: .6, tip: 'Eraser'}, act: function(){
+			sp.drag(0);
+			sp.txtedit(0);
+			sp.pencolor(sp.bgcolor());
+			toolBar.collapseall();
+			toolBar.root.children().eq(0).hide();
+			toolBar.root.children().eq(1).hide();
+		}},
+		{btn: {w: w, h: h, icon: svgicon.textinput, iconcolor: 'white', borderwidth: 3, bordercolor: 'white', borderradius: 2, bgcolor: 'red', resize: .6, tip: 'Text Input'},	act: function(){
+			sp.drag(0);
+			sp.txtedit(1);
+			toolBar.collapseall();
+			toolBar.root.children().show();
+			toolBar.root.children().eq(1).hide();
+		}},
+		{btn: {w: w, h: h, icon: svgicon.move, iconcolor: 'white', borderwidth: 3, bordercolor: 'white', borderradius: 2, bgcolor: 'red', resize: .6, tip: 'Move Canvas'}, act: function(){
+			sp.drag(0);
+			sp.drag(1);
+			toolBar.collapseall();
+			toolBar.root.children().eq(0).hide();
+			toolBar.root.children().eq(1).hide();
+		}},
+		{btn: {w: w, h: h, icon: svgicon.geometrical, iconcolor: 'white', borderwidth: 3, bordercolor: 'white', borderradius: 2, bgcolor: 'red', resize: .6, tip: 'Draw Geometrics'}, act: function(){
+			sp.drag(0);
+			sp.drag(0);
+			toolBar.collapseall();
+			toolBar.root.children().show();
+			toolBar.root.children().eq(1).hide()
+		}}
+	];
+	var mainActBtn = [{sublist: actBtns, direction: subdir}];
+	var actList = {rootlist: mainActBtn, direction: rootdir};
+	var actMenu = new Gatherhub.BtnMenu(actList);
+	actMenu.autocollapse = true;
+	if (topmenu) actMenu.root.css({'position': 'absolute', 'bottom': 0, 'right': 5});
+	else actMenu.root.css({'position': 'absolute', 'bottom': 5, 'right': 0});
+	
+	
 	$(window).on('resize', function(){
 		toolBar.collapseall();
 		if (topmenu) {
@@ -351,17 +404,18 @@ function appendUser(elem, peerid, uname, color) {
 
 function appendMsg(elem, pid, sender, msg, color, tid) {
 	console.log(sender + '(' + tid + '): ' + msg);
+	var msg = Autolinker.link(msg, {newWindo: true, stripPrefix: false});
 	var lr = sender == 'Me' ? 'right' : 'left';
 	var prev = $(elem).children().last();
 	var prevlr = prev.length ? prev.children().last().css('float') : '';
 	var pp = $('<div>'); 
 	if (prevlr.length) pp.css('clear', prevlr);
 	var ph = $('<div class="panel-heading" style="color: #000; margin: 0; padding: 0; text-align: ' + lr + '">');
-	var pb = $('<div class="panel-body" style="float: ' + lr + '; max-width: 290px; word-break: break-all; border-radius: 5px; margin: 5px; font-weight: bold; background-color: #FFF; border-width: 2px; border-style: solid; border-color: ' + color + ';">');
+	var pb = $('<div class="panel-body" style="float: ' + lr + '; max-width: 290px; word-break: break-all; border-radius: 5px; margin: 5px; font-weight: bold; background-color: #FFF;">');
 	if (lr == 'left') pb.addClass('leftbubble');
 	else pb.addClass('rightbubble');
 	
-	pp.attr('tid', tid);
+	pp.attr('tid', tid).append($('<br>'));
 	ph.html(sender + ':').appendTo(pp);
 	pb.html(msg).appendTo(pp);
 	var tgt = $(elem).children().last();
