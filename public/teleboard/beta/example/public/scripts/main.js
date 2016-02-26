060a4b58e2e0b55f955c853493a0f81dc76f606c
Quark Li <quarkli@gmail.com>
2016-02-27 00:35:31 +0800

    remove unused files

--- public/teleboard/beta/example/public/scripts/main.js
+++
@@ -1,9 +0,0 @@
-
-$(function(){
-	var sp = new Gatherhub.SketchPad();
-	sp.appendto('#layer2');
-
-	$(window).on('resize', function(){
-		sp.width($(window).width()).height($(window).height());
-	});
-});
